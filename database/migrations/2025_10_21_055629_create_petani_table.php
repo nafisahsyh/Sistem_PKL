d36c07f6daedc1d36927a8cec1a33749824f3f46
nafisahsyh <nafisahsayyidah4@gmail.com>
2025-11-21 09:56:56 +0800

    perubahan tabel petani

--- database/migrations/2025_10_21_055629_create_petani_table.php
+++ database/migrations/2025_10_21_055629_create_petani_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('petani', function (Blueprint $table) {
             $table->bigIncrements('id_petani');
             $table->string('nomor_anggota_plasma', 100)->unique()->nullable();
-            $table->string('nomor_anggota_koperasi', 100)->unique()->nullable();
+            $table->string('nomor_anggota_koperasi', 100)->nullable();
             $table->string('NIK', 16)->unique()->nullable();
             $table->string('nama', 255);
             $table->string('alamat', 255)->nullable();
